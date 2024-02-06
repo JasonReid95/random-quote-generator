60ce9c587c0e66f9f326dd911dd4d3dd7d4aa870
Jason Reid <jas.reid.95@gmail.com>
2024-02-05 23:46:11 -0600

    Added functionality for a loading indicator and disabled button  while quote is being generated.

--- random-quote.js
+++ random-quote.js
@@ -1,6 +1,9 @@
 // Creates newQuoteButton variable and stores a reference to the new quote button in index.html
 const newQuoteButton = document.querySelector("#js-new-quote");
 
+// Creates jsLoadIndicator variable and stores a reference to the js load indicator in index.html
+const jsLoadIndicator = document.querySelector("#js-load-indicator");
+
 // Creates endpoint variable and stores the address to "What Does Trump Think?"
 const endpoint = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
 
@@ -12,6 +15,10 @@ function displayQuote(quote) {
 
 // Asynchronous function that displays a new quote with flow and error handling
 async function getQuote() {
+// Removes hidden class from js load indicator when new quote is generating
+    jsLoadIndicator.classList.remove("hidden");
+// Disables new quote button while new quote is generating
+    newQuoteButton.disabled = true;
     try {
         const response = await fetch(endpoint);
         if (!response.ok) {
@@ -20,12 +27,16 @@ async function getQuote() {
         const json = await response.json();
         displayQuote(json.message);
     }
-    catch (err) {
-        console.log(err);
+    catch {
         alert("Failed to fetch new quote.");
+    }
+    finally {
+// Re-enables new quote button whether new quote is generated or not
+        newQuoteButton.disabled = false;
+// Adds hidden class back to js load indicator whether new quote is generated or not
+        jsLoadIndicator.classList.add("hidden");
     }
 }
 
 // Adds an event listener that invokes getQuote function when newQuoteButton is clicked 
 newQuoteButton.addEventListener("click", getQuote);
-
